217c1abc834a62c3090236f13748a6dd0f0f6f15
AcebedoParajon <acebedo.parajon@gmail.com>
2019-01-07 21:09:46 +0100

    revertir version

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,23 @@
 
 
 export default {
+  created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      return new Promise(function (resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch(logout)
+        }
+        throw err;
+      });
+    });
+  },
 
   computed: {
     isLoggedIn: function() {
       return this.$store.getters.isLoggedIn;
     }
   },
+
   methods: {
     logout: function() {
       this.$store.dispatch("logout").then(() => {
@@ -39,6 +50,7 @@ export default {
     }
   }
 };
+
 </script>
 
 <style>
